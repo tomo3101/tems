8ac197c401b9d8d48638bb2576470ac73ec7fe8d
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-12-19 04:27:27 +0000

    fix: Drizzleの設定から不要なスキーマパスを削除

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'mysql',
-  schema: ['./dist/src/infra/db/schema', './dist/src/infra/db/relation'],
+  schema: ['./dist/src/infra/db/schema'],
   out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
