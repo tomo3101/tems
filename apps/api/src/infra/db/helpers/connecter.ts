25de396ec908f69501dc29a349f154e6c3d88bcd
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-12-26 18:10:23 +0900

    fix: DATABASE_URLが未定義の場合にエラーをスローするように修正

--- apps/api/src/infra/db/helpers/connecter.ts
+++ apps/api/src/infra/db/helpers/connecter.ts
@@ -7,7 +7,13 @@ import * as reservationsSchema from '../schemas/reservations.js';
 import * as temporaryMembersSchema from '../schemas/temporaryMembers.js';
 import * as tokensSchema from '../schemas/tokens.js';
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+const databaseUrl = process.env.DATABASE_URL;
+
+if (databaseUrl === undefined) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+export const db = drizzle(databaseUrl, {
   schema: {
     ...adminsSchema,
     ...eventsSchema,
