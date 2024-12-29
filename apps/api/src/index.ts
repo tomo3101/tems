1414dd662587771640b53ee52bf0f6c8dfaac5ff
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-12-29 09:12:29 +0000

    add: OpenAPIドキュメントとSwagger UIの設定

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,22 @@
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { swaggerUI } from '@hono/swagger-ui';
+import { OpenAPIHono } from '@hono/zod-openapi';
 
-const app = new Hono();
+const app = new OpenAPIHono();
+const api = app.basePath('/api/v1');
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
+
+api.doc('/docs', {
+  openapi: '3.0.0',
+  info: {
+    title: 'TEMS API',
+    version: process.env.npm_package_version!,
+  },
+});
+api.get('/docs/ui', swaggerUI({ url: '/api/v1/docs' }));
 
 const port = 3001;
 console.log(`Server is running on http://localhost:${port}`);
