b59c14d602ccfbb2f59aeefd9a32530ac8bc9a6d
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2024-12-29 14:48:56 +0000

    add: ログアウトスキーマを追加

---
+++ apps/api/src/application/schemas/logoutSchema.ts
@@ -0,0 +1,15 @@
+import { z } from '@hono/zod-openapi';
+
+// ログアウト用ボディスキーマ
+export const deleteLogoutBodySchema = z.object({
+  refresh_token: z.string().openapi({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoiYWRtaW4iLCJleHAiOjE3MDQxNjQ0MDB9.bVHuEZqLypjmqyONRheFEqEwt4KuIYht-VJ0cPg3b0E',
+  }),
+});
+
+// ログアウト用レスポンススキーマ
+export const deleteLogoutResponseSchema = z.object({
+  user_id: z.number().int().positive().openapi({ example: 1 }),
+  role: z.enum(['admin', 'member']).openapi({ example: 'admin' }),
+});
