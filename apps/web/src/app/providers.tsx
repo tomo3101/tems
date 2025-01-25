5b7289eef1f80cd7a83f18e2bad3d1975d0865b0
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2025-01-25 13:46:02 +0000

    add: Providersコンポーネントにnext-themesのProviderを追加

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,6 +1,18 @@
 'use client';
-import { HeroUIProvider } from '@heroui/react';
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+import { HeroUIProvider } from '@heroui/react';
+import type { ThemeProviderProps } from 'next-themes';
+import { ThemeProvider as NextThemasProvider } from 'next-themes';
+
+interface ProvidersProps {
+  children: React.ReactNode;
+  themaProps?: ThemeProviderProps;
+}
+
+export function Providers({ children, themaProps }: ProvidersProps) {
+  return (
+    <HeroUIProvider>
+      <NextThemasProvider {...themaProps}>{children}</NextThemasProvider>
+    </HeroUIProvider>
+  );
 }
