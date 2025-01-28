f61305de5a39bc3bcccc5f40b054051fb37a732e
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2025-01-28 05:41:49 +0000

    add: 認証ルートを定義するファイルを追加

---
+++ apps/web/src/auth/routes.ts
@@ -0,0 +1,5 @@
+export const publicRoutes = ['/'];
+export const authRoutes = ['/login', '/register'];
+export const ADMIN_BASE_ROUTE = '/admin';
+export const ERROR_SESSION_ROUTE = '/error/session';
+export const DEFAULT_LOGIN_REDIRECT = '/';
