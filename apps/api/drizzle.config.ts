e02bf20f2237d69609e7fb0c2ac17443162a6c28
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2025-02-14 13:48:36 +0000

    fix: drizzle.configのschemaパスの修正

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'mysql',
-  schema: './dist/src/infra/db/schemas',
+  schema: './dist/src/infra/db/schemas/*.js',
   out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
