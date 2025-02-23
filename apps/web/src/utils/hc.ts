0e719db6da488bdcded638e9221d5b3e66b8b423
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2025-02-23 09:48:37 +0000

    add: Zodスキーマを使用して型定義を追加

--- apps/web/src/utils/hc.ts
+++ apps/web/src/utils/hc.ts
@@ -1,4 +1,12 @@
 import { hcWithType } from 'api/hc';
+import { adminSchema } from 'api/schema/adminSchema';
+import { eventSchema } from 'api/schema/eventSchema';
+import { memberSchema } from 'api/schema/memberSchema';
+import {
+  reservationSchema,
+  reservationWithEventSchema,
+} from 'api/schema/reservationSchema';
+import { z } from 'zod';
 
 export const hcWithAuth = (accessToken: string) => {
   return hcWithType('http://localhost:3001/', {
@@ -7,3 +15,12 @@ export const hcWithAuth = (accessToken: string) => {
     },
   });
 };
+
+export type Event = z.infer<typeof eventSchema>;
+
+export type Reservation = z.infer<typeof reservationSchema>;
+export type ReservationWithEvent = z.infer<typeof reservationWithEventSchema>;
+
+export type Member = z.infer<typeof memberSchema>;
+
+export type Admin = z.infer<typeof adminSchema>;
