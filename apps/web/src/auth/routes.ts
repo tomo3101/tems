97a1077297375f02b09075bb05b307d11177e464
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2025-02-24 10:59:23 +0000

    fix: 待ち状況ルートの認証設定を修正

--- apps/web/src/auth/routes.ts
+++ apps/web/src/auth/routes.ts
@@ -1,5 +1,6 @@
-export const publicRoutes = ['/', '/blog', '/news', '/status', '/waiting'];
+export const publicRoutes = ['/', '/blog', '/news', '/status'];
 export const authRoutes = ['/login', '/login/admin', '/register'];
+export const WAITING_BASE_ROUTE = '/waiting';
 export const ADMIN_BASE_ROUTE = '/admin';
 export const ERROR_BASE_ROUTE = '/error';
 export const ERROR_SESSION_ROUTE = '/error/session';
