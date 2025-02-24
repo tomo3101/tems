d1059d8f099e770cb72b62bf9d06bdcc5833a0f9
tomo3101 <85130628+tomo3101@users.noreply.github.com>
2025-02-24 13:07:46 +0000

    fix: hcWithTypeでのフェッチ処理の修正
    キャッシュが有効になっていたためNextBuild時にエラー発生

--- apps/web/src/app/(header)/waiting/page.tsx
+++ apps/web/src/app/(header)/waiting/page.tsx
@@ -1,7 +1,46 @@
 import { EventWaitingStatusCard } from '@/components/layout/event/card';
 import { Card, CardBody, CardHeader } from '@heroui/card';
+import { hcWithType } from 'api/hc';
 
-export default function WaitingPage() {
+export default async function WaitingPage() {
+  const client = hcWithType('http://localhost:3001', {
+    fetch: (input: RequestInfo | URL, requestInit?: RequestInit) =>
+      fetch(input, {
+        cache: 'no-cache',
+        ...requestInit,
+      }),
+  });
+
+  const rowEventsResponce = await client.api.v1.events.$get({
+    query: {},
+  });
+
+  const rowReservationsResponce = await client.api.v1.reservations.$get({
+    query: {},
+  });
+
+  if (!rowEventsResponce.ok || !rowReservationsResponce.ok) {
+    return (
+      <>
+        <h1 className="text-4xl font-medium">イベント待ち状況</h1>
+
+        <Card>
+          <CardHeader className="flex justify-center">
+            <h2 className="text-3xl font-medium">体験運転スケジュール</h2>
+          </CardHeader>
+          <CardBody>
+            <p className="text-center text-2xl font-medium">
+              体験運転スケジュールの取得に失敗しました
+            </p>
+          </CardBody>
+        </Card>
+      </>
+    );
+  }
+
+  const events = await rowEventsResponce.json();
+  const reservations = await rowReservationsResponce.json();
+
   return (
     <>
       <h1 className="text-4xl font-medium">イベント待ち状況</h1>
@@ -11,7 +50,7 @@ export default function WaitingPage() {
           <h2 className="text-3xl font-medium">体験運転スケジュール</h2>
         </CardHeader>
         <CardBody>
-          <EventWaitingStatusCard />
+          <EventWaitingStatusCard events={events} reservations={reservations} />
         </CardBody>
       </Card>
     </>
